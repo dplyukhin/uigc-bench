4eb91c6c0409590d3881a67fcf290a4793c2cfb3
Dan P <dplyukhin@gmail.com>
2023-07-21 14:33:18 -0500

    WIP

--- src/main/scala/randomworkers/RandomWorkers.scala
+++ src/main/scala/randomworkers/RandomWorkers.scala
@@ -4,19 +4,21 @@ import akka.actor.typed.scaladsl.TimerScheduler
 import com.typesafe.config.{Config, ConfigFactory}
 import common.{CborSerializable, ClusterBenchmark}
 import edu.illinois.osl.akka.gc.interfaces.{Message, NoRefs, RefobLike}
-import edu.illinois.osl.akka.gc.{AbstractBehavior, ActorContext, ActorRef, Behavior, Behaviors, unmanaged}
-import randomworkers.RandomWorkers.Manager.Cfg
+import edu.illinois.osl.akka.gc.protocols.monotone.GCMessage
+import edu.illinois.osl.akka.gc.{AbstractBehavior, ActorContext, ActorFactory, ActorRef, Behavior, Behaviors, unmanaged}
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.duration.DurationInt
 
 object RandomWorkers {
 
   trait Protocol extends CborSerializable with Message
-  private case class LearnPeers(peers: Seq[ActorRef[Protocol]]) extends Protocol {
+  private case class LearnPeers(peers: Seq[unmanaged.ActorRef[Protocol]]) extends Protocol {
     override def refs: Iterable[RefobLike[Nothing]] = peers
   }
-  private case object NewWork extends Protocol with NoRefs
+  private case object Ping extends Protocol with NoRefs
+  private case class Work(work: Array[Short]) extends Protocol with NoRefs
   private case class Acquaint(workers: Seq[ActorRef[Protocol]]) extends Protocol {
     override def refs: Iterable[RefobLike[Nothing]] = workers
   }
@@ -63,29 +65,40 @@ object RandomWorkers {
         items.view.slice(i, i + 1).head
       }
 
+      def select[T](items: Iterable[T], bound: Int): Iterable[T] = {
+        if (items.isEmpty) return Nil
+        val numItems = rng.nextInt(0, bound + 1)
+        (1 to numItems).map(_ => select(items))
+      }
+
       def randNat(bound: Int): Int =
         rng.nextInt(0, bound)
+
+      def coinFlip(): Int =
+        rng.nextBoolean()
     }
 
     def leader(
                benchmark: ActorRef[ClusterBenchmark.Protocol[Protocol]],
-               workerNodes: Map[String, ActorRef[Protocol]],
+               workerNodes: Map[String, unmanaged.ActorRef[Protocol]],
                isWarmup: Boolean
              ): unmanaged.Behavior[Protocol] =
       Behaviors.setupRoot[Protocol] { ctx =>
         benchmark ! ClusterBenchmark.OrchestratorReady()
 
-        val peers = workerNodes.values.toSeq :+ ctx.self
-        for (worker <- workerNodes.values)
-          worker ! LearnPeers(peers)
+        // TODO Need to get a version of this actor's actorref to add to the peers
+        // TODO Also nee
+        val peers = workerNodes.values.toSeq //:+ ctx.self.rawActorRef.unsafeUpcast[GCMessage[Protocol]]
+        for (peer <- peers)
+          peer ! LearnPeers(peers)
 
         startManager(peers)
       }
 
-    private def startManager(peers: Seq[ActorRef[Protocol]]): unmanaged.Behavior[Protocol] =
+    private def startManager(peers: Seq[unmanaged.ActorRef[Protocol]]): unmanaged.Behavior[Protocol] =
       Behaviors.withTimers[Protocol] { timers =>
         val cfg = new Cfg()
-        timers.startTimerAtFixedRate((), NewWork, (1000000000 / cfg.reqsPerSecond).nanos)
+        timers.startTimerAtFixedRate((), Ping, (1000000000 / cfg.reqsPerSecond).nanos)
         Behaviors.setupRoot[Protocol](ctx =>
           new ManagerActor(ctx, timers, cfg)
         )
@@ -95,8 +108,8 @@ object RandomWorkers {
 
       private val rng = new Random()
       private var peers: Seq[ActorRef[Protocol]] = Seq()
-      private val localWorkers: ArrayBuffer[ActorRef[Protocol]] = new ArrayBuffer()
-      private val remoteWorkers: ArrayBuffer[ActorRef[Protocol]] = new ArrayBuffer()
+      private val localWorkers: mutable.HashSet[ActorRef[Protocol]] = mutable.HashSet()
+      private val remoteWorkers: mutable.HashSet[ActorRef[Protocol]] = mutable.HashSet()
 
       override def onMessage(msg: Protocol): Behavior[Protocol] = msg match {
         case LearnPeers(peers) =>
@@ -104,51 +117,55 @@ object RandomWorkers {
           this
 
         case Acquaint(workers) =>
-          remoteWorkers.appendAll(workers)
+          remoteWorkers.addAll(workers)
           this
 
-        case NewWork =>
+        case Ping =>
           if (rng.roll(config.managerProbSpawn)) {
             for (i <- 1 to rng.randNat(config.maxSpawnsInOneTurn))
-              localWorkers.append(ctx.spawnAnonymous(Worker()))
+              localWorkers.add(ctx.spawnAnonymous(Worker()))
           }
-          if (rng.roll(config.managerProbLocalSend) && remoteWorkers.nonEmpty) {
-            // TODO Create work
+          if (rng.roll(config.managerProbLocalSend) && localWorkers.nonEmpty) {
+            val work = Array.tabulate[Short](rng.randNat(config.maxWorkSizeBytes))(i => i.toShort)
             for (i <- 1 to rng.randNat(config.maxSendsInOneTurn))
-              rng.select(remoteWorkers) ! NewWork
+              rng.select(localWorkers) ! Work(work)
           }
           if (rng.roll(config.managerProbRemoteSend) && remoteWorkers.nonEmpty) {
-            // TODO Create work
+            val work = Array.tabulate[Short](rng.randNat(config.maxWorkSizeBytes))(i => i.toShort)
             for (i <- 1 to rng.randNat(config.maxSendsInOneTurn))
-              rng.select(remoteWorkers) ! NewWork
+              rng.select(remoteWorkers) ! Work(work)
           }
           if (rng.roll(config.managerProbLocalAcquaint) && localWorkers.nonEmpty) {
-            val numAcqs = rng.randNat(config.maxAcqsInOneMsg + 1)
-            val acqs = (1 to numAcqs).map(_ => rng.select(localWorkers))
+            val acqs = rng.select(localWorkers, config.maxAcqsInOneMsg).toSeq
             val owner = rng.select(localWorkers)
-            // TODO Create refs
-            owner ! Acquaint(acqs)
+            val refs = acqs.map(acq => ctx.createRef(acq, owner))
+            owner ! Acquaint(refs)
           }
-          if (rng.roll(config.managerProbRemoteAcquaint) && remoteWorkers.nonEmpty && localWorkers.nonEmpty) {
-            val numAcqs = rng.randNat(config.maxAcqsInOneMsg + 1)
-            val acqs = (1 to numAcqs).map(_ => rng.select(remoteWorkers))
+          if (rng.roll(config.managerProbRemoteAcquaint) && localWorkers.nonEmpty) {
+            val acqs = rng.select(remoteWorkers, config.maxAcqsInOneMsg).toSeq
             val owner = rng.select(localWorkers)
-            // TODO Create refs
-            owner ! Acquaint(acqs)
+            val refs = acqs.map(acq => ctx.createRef(acq, owner))
+            owner ! Acquaint(refs)
           }
-          if (rng.roll(config.managerProbPublishWorker) && localWorkers.nonEmpty) {
-            val numAcqs = rng.randNat(config.maxAcqsInOneMsg + 1)
-            val acqs = (1 to numAcqs).map(_ => rng.select(localWorkers))
+          if (rng.roll(config.managerProbPublishWorker) && peers.nonEmpty) {
+            val acqs = rng.select(localWorkers, config.maxAcqsInOneMsg).toSeq
             val peer = rng.select(peers)
-            // TODO Create refs
-            peer ! Acquaint(acqs)
+            val refs = acqs.map(acq => ctx.createRef(acq, peer))
+            peer ! Acquaint(refs)
           }
           if (rng.roll(config.managerProbDeactivate)) {
-            val numDeacs = rng.randNat(config.maxDeactivatedInOneTurn + 1)
-            for (_ <- 1 to numDeacs)
-              () // TODO
+            val locals = rng.select(localWorkers, config.maxDeactivatedInOneTurn).toSeq
+            val remotes = rng.select(remoteWorkers, config.maxDeactivatedInOneTurn).toSeq
+            ctx.release(locals)
+            ctx.release(remotes)
+            for (worker <- locals)
+              localWorkers.remove(worker)
+            for (worker <- remotes)
+              remoteWorkers.remove(worker)
           }
           if (rng.roll(config.managerProbDeactivateAll)) {
+            ctx.release(localWorkers)
+            ctx.release(remoteWorkers)
             localWorkers.clear()
             remoteWorkers.clear()
           }
@@ -168,7 +185,7 @@ object RandomWorkers {
   }
 
   object Worker {
-    def apply(): Behavior[Protocol] =
+    def apply(): ActorFactory[Protocol] =
       Behaviors.setup[Protocol] { ctx =>
         new WorkerActor(ctx)
       }
