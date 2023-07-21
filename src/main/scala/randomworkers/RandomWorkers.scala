95348b264a339e3d6e2c59b883f3f9b4fcd45ed0
Dan P <dplyukhin@gmail.com>
2023-07-21 17:56:59 -0500

    WIP

--- src/main/scala/randomworkers/RandomWorkers.scala
+++ src/main/scala/randomworkers/RandomWorkers.scala
@@ -1,6 +1,6 @@
 package randomworkers
 
-import akka.actor.typed.scaladsl
+import akka.actor.typed.{SpawnProtocol, scaladsl}
 import akka.actor.typed.scaladsl.TimerScheduler
 import com.typesafe.config.ConfigFactory
 import common.{CborSerializable, ClusterBenchmark}
@@ -18,7 +18,7 @@ object RandomWorkers {
     override def refs: Iterable[RefobLike[Nothing]] = peers
   }
   private case object Ping extends Protocol with NoRefs
-  private case class Work(work: Array[Short]) extends Protocol with NoRefs
+  private case class Work(work: Array[Byte]) extends Protocol with NoRefs
   private case class Acquaint(workers: Seq[ActorRef[Protocol]]) extends Protocol {
     override def refs: Iterable[RefobLike[Nothing]] = workers
   }
@@ -76,44 +76,61 @@ object RandomWorkers {
 
   private object Manager {
 
-    def managerManager(
+    def leader(
                benchmark: unmanaged.ActorRef[ClusterBenchmark.Protocol[Protocol]],
-               workerNodes: Map[String, unmanaged.ActorRef[Protocol]],
+               workerNodes: Map[String, unmanaged.ActorRef[SpawnProtocol.Command]],
                isWarmup: Boolean
-             ): unmanaged.Behavior[Protocol] = {
-      scaladsl.Behaviors.setup[Protocol] { ctx =>
-        // This actor manages the lead manager; it spawns the manager and then
-        // sends every manager a reference to every other manager.
+             ): unmanaged.Behavior[SpawnProtocol.Command] = {
+      scaladsl.Behaviors.setup[SpawnProtocol.Command] { ctx =>
         benchmark ! ClusterBenchmark.OrchestratorReady()
 
-        val leader = ctx.spawnAnonymous(manager())
+        ctx.spawn(leadManager(workerNodes.values), "manager0")
 
-        val peers = workerNodes.values.toSeq :+ leader
-        for (peer <- peers)
-          peer ! LearnPeers(peers.filter(_ != peer))
-
-        scaladsl.Behaviors.receiveMessage[Protocol] { _ =>
-          scaladsl.Behaviors.same
-        }
+        SpawnProtocol()
       }
     }
 
-    def manager(): unmanaged.Behavior[Protocol] =
+    def follower(): unmanaged.Behavior[SpawnProtocol.Command] = {
+      scaladsl.Behaviors.setup[SpawnProtocol.Command] { ctx =>
+        SpawnProtocol()
+      }
+    }
+
+    def leadManager(workerNodes: Iterable[unmanaged.ActorRef[SpawnProtocol.Command]]): unmanaged.Behavior[Protocol] =
       Behaviors.withTimers[Protocol] { timers =>
         val config = new Config()
         timers.startTimerAtFixedRate((), Ping, (1000000000 / config.reqsPerSecond).nanos)
 
         Behaviors.setupRoot[Protocol] { ctx =>
-          new ManagerActor(ctx, timers, config)
+          new ManagerActor(ctx, config, workerNodes)
         }
       }
 
-    private class ManagerActor(ctx: ActorContext[Protocol], timers: TimerScheduler[Protocol], config: Config) extends AbstractBehavior[Protocol](ctx) {
+    def followerManager(): ActorFactory[Protocol] =
+      Behaviors.setup[Protocol] { ctx =>
+        val config = new Config()
+        new ManagerActor(ctx, config, Nil)
+      }
+
+    private class ManagerActor(ctx: ActorContext[Protocol], config: Config, workerNodes: Iterable[unmanaged.ActorRef[SpawnProtocol.Command]]) extends AbstractBehavior[Protocol](ctx) {
 
       private val rng = new Random()
       private var peers: Seq[ActorRef[Protocol]] = Seq()
       private val localWorkers: mutable.HashSet[ActorRef[Protocol]] = mutable.HashSet()
       private val remoteWorkers: mutable.HashSet[ActorRef[Protocol]] = mutable.HashSet()
+
+      // Spawn the other managers and send those managers references to one another
+      if (workerNodes.nonEmpty) {
+        peers = (for ((node, i) <- workerNodes.zipWithIndex) yield
+          ctx.spawnRemote(followerManager(), node, s"manager${i+1}")
+        ).toSeq
+        for (peer <- peers) {
+          val refs = (peers :+ ctx.self).filter(_ != peer).map(target =>
+            ctx.createRef(target, peer)
+          )
+          peer ! LearnPeers(refs)
+        }
+      }
 
       override def onMessage(msg: Protocol): Behavior[Protocol] = msg match {
         case LearnPeers(peers) =>
@@ -127,7 +144,7 @@ object RandomWorkers {
         case Ping =>
           if (rng.roll(config.managerProbSpawn)) {
             for (i <- 1 to rng.randNat(config.maxSpawnsInOneTurn))
-              localWorkers.add(ctx.spawnAnonymous(Worker()))
+              localWorkers.add(ctx.spawnAnonymous(Worker(config, rng)))
           }
           if (rng.roll(config.managerProbLocalSend) && localWorkers.nonEmpty) {
             val work = rng.genData(config.maxWorkSizeBytes)
@@ -180,24 +197,59 @@ object RandomWorkers {
   }
 
   object Worker {
-    def apply(): ActorFactory[Protocol] =
+    def apply(config: Config, rng: Random): ActorFactory[Protocol] =
       Behaviors.setup[Protocol] { ctx =>
-        new WorkerActor(ctx)
+        new WorkerActor(ctx, config, rng)
       }
 
-    private class WorkerActor(ctx: ActorContext[Protocol]) extends AbstractBehavior[Protocol](ctx) {
+    private class WorkerActor(ctx: ActorContext[Protocol], config: Config, rng: Random) extends AbstractBehavior[Protocol](ctx) {
+      private val acquaintances: mutable.HashSet[ActorRef[Protocol]] = mutable.HashSet()
+      private val state: Array[Byte] = Array.tabulate[Byte](config.maxWorkSizeBytes)(i => i.toByte)
+
       override def onMessage(msg: Protocol): Behavior[Protocol] = msg match {
-        case Ping => this
+        case Acquaint(workers) =>
+          acquaintances.addAll(workers)
+          this
+        case Work(work) =>
+          for (i <- work.indices) {
+            state(i) = (state(i) * work(i)).toByte
+          }
+          if (rng.roll(config.workerProbSpawn)) {
+            for (i <- 1 to rng.randNat(config.maxSpawnsInOneTurn))
+              acquaintances.add(ctx.spawnAnonymous(Worker(config, rng)))
+          }
+          if (rng.roll(config.workerProbSend) && acquaintances.nonEmpty) {
+            val work = state
+            for (i <- 1 to rng.randNat(config.maxSendsInOneTurn))
+              rng.select(acquaintances) ! Work(work)
+          }
+          if (rng.roll(config.workerProbAcquaint) && acquaintances.nonEmpty) {
+            val acqs = rng.select(acquaintances, config.maxAcqsInOneMsg).toSeq
+            val owner = rng.select(acquaintances)
+            val refs = acqs.map(acq => ctx.createRef(acq, owner))
+            owner ! Acquaint(refs)
+          }
+          if (rng.roll(config.workerProbDeactivate)) {
+            val locals = rng.select(acquaintances, config.maxDeactivatedInOneTurn).toSeq
+            ctx.release(locals)
+            for (worker <- locals)
+              acquaintances.remove(worker)
+          }
+          if (rng.roll(config.workerProbDeactivateAll)) {
+            ctx.release(acquaintances)
+            acquaintances.clear()
+          }
+          this
       }
     }
   }
 
   def main(args: Array[String]): Unit =
     ClusterBenchmark(
-      Manager.managerManager,
+      Manager.leader,
       Map(
-        "worker1" -> Manager.manager(),
-        "worker2" -> Manager.manager(),
+        "worker1" -> Manager.follower(),
+        "worker2" -> Manager.follower(),
       )
     ).runBenchmark(args)
 }
