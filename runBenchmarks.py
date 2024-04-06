de038b81bc633ec45a51e97697af7729acbb56c4
Dan Plyukhin <dplyukhin@gmail.com>
2024-04-06 12:22:36 -0500

    Added a couple more modes for the benchmark

--- runBenchmarks.py
+++ runBenchmarks.py
@@ -1,27 +1,46 @@
 import subprocess
 import os
 import time
+import sys
+
+default_mode = []
+
+big_data_mode = [
+    "-Drandom-workers.max-work-size-in-bytes=5120"
+]
+
+streaming_mode = [
+    "-Drandom-workers.max-work-size-in-bytes=5120",
+    "-Drandom-workers.wrk-probabilities.spawn=0",
+    "-Drandom-workers.wrk-probabilities.acquaint=0",
+    "-Drandom-workers.mgr-probabilities.spawn=0.01",
+    "-Drandom-workers.mgr-probabilities.local-acquaint=0",
+    "-Drandom-workers.mgr-probabilities.publish-worker=1",
+    "-Drandom-workers.mgr-probabilities.deactivate=0",
+    "-Drandom-workers.total-queries=20000",
+]
 
 
-def run_benchmark(reqs_per_second, delta_graph_size):
+def run_benchmark(reqs_per_second, delta_graph_size, output_dir, mode):
     filename = f"workers-rps-{reqs_per_second}-dgs-{delta_graph_size}"
 
     # Add JFR options to SBT opts, saving the old value to be restored later.
     original_sbt_opts = os.environ.get("SBT_OPTS", "")
 
-    with (open(f'{filename}.log', 'w') as log):
+    with open(f'{output_dir}/{filename}.log', 'w') as log:
 
         processes = []
         for role in ["orchestrator", "manager1", "manager2"]:
             os.environ["SBT_OPTS"] = original_sbt_opts + \
-              f" -XX:StartFlightRecording=filename={filename}-{role}.jfr,dumponexit=true"
+              f" -XX:StartFlightRecording=filename={output_dir}/{filename}-{role}.jfr,dumponexit=true"
 
             print(f"Starting {role}")
             process = subprocess.Popen(
-                ["sbt", "-Duigc.crgc.num-nodes=3",
+                ["sbt", "-J-Xmx2G", "-Duigc.crgc.num-nodes=3",
                  f"-Duigc.crgc.delta-graph-size={delta_graph_size}",
-                 f"-Drandom-workers.reqs-per-second={reqs_per_second}",
-                 f"runMain randomworkers.RandomWorkers {role} 0.0.0.0 0.0.0.0"],
+                 f"-Drandom-workers.reqs-per-second={reqs_per_second}"] +
+                mode +
+                [f"runMain randomworkers.RandomWorkers {role} 0.0.0.0 0.0.0.0"],
                 stdout=log
             )
             processes.append(process)
@@ -35,11 +54,32 @@ def run_benchmark(reqs_per_second, delta_graph_size):
     os.environ["SBT_OPTS"] = original_sbt_opts
 
 
-def run_benchmarks():
-    for reqs_per_second in [100, 200, 300, 400, 500]:
-        for delta_graph_size in [64, 128, 256, 512, 1024]:
-            run_benchmark(reqs_per_second, delta_graph_size)
+def run_benchmarks(output_dir, mode):
+    for reqs_per_second in [200, 300, 400, 500]:
+        for delta_graph_size in [128, 256, 512, 1024, 2048]:
+            run_benchmark(reqs_per_second, delta_graph_size, output_dir, mode)
 
 
 if __name__ == '__main__':
-    run_benchmarks()
+    # Get the output directory from the command line
+    if len(sys.argv) != 3:
+        print("Usage: python runBenchmarks.py <output-directory> <mode>")
+        sys.exit(1)
+
+    output_dir = sys.argv[1]
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    mode = None
+    mode_arg = sys.argv[2]
+    if mode_arg == "default":
+        mode = default_mode
+    elif mode_arg == "big-data":
+        mode = big_data_mode
+    elif mode_arg == "streaming":
+        mode = streaming_mode
+    else:
+        print("Invalid mode. Choose one of: default, big-data, streaming")
+        sys.exit(1)
+
+    run_benchmarks(output_dir, mode)
