81ba3b7a456745fb7b2373c26909df18e60b5881
Dan Plyukhin <dplyukhin@gmail.com>
2024-11-05 23:28:31 +0000

    Converted to argparse, added memory profiling, removed DGS parameter

--- runBenchmarks.py
+++ runBenchmarks.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import time
 import sys
+import argparse
 
 default_mode = []
 
@@ -21,8 +22,8 @@ streaming_mode = [
 ]
 
 
-def run_benchmark(reqs_per_second, delta_graph_size, output_dir, mode):
-    filename = f"workers-rps-{reqs_per_second}-dgs-{delta_graph_size}"
+def run_benchmark(reqs_per_second, output_dir, mode):
+    filename = f"workers-rps-{reqs_per_second}"
 
     # Add JFR options to SBT opts, saving the old value to be restored later.
     original_sbt_opts = os.environ.get("SBT_OPTS", "")
@@ -31,13 +32,15 @@ def run_benchmark(reqs_per_second, delta_graph_size, output_dir, mode):
 
         processes = []
         for role in ["orchestrator", "manager1", "manager2"]:
+            jfr_file = f"{output_dir}/{filename}-{role}.jfr"
+            jfr_settings = 'profile.jfc'
+
             os.environ["SBT_OPTS"] = original_sbt_opts + \
-              f" -XX:StartFlightRecording=filename={output_dir}/{filename}-{role}.jfr,dumponexit=true"
+              f" -XX:StartFlightRecording=filename={jfr_file},settings={jfr_settings},dumponexit=true"
 
             print(f"Starting {role}")
             process = subprocess.Popen(
                 ["sbt", "-J-Xmx2G", "-Duigc.crgc.num-nodes=3",
-                 f"-Duigc.crgc.delta-graph-size={delta_graph_size}",
                  f"-Drandom-workers.reqs-per-second={reqs_per_second}"] +
                 mode +
                 [f"runMain randomworkers.RandomWorkers {role} 0.0.0.0 0.0.0.0"],
@@ -55,31 +58,27 @@ def run_benchmark(reqs_per_second, delta_graph_size, output_dir, mode):
 
 
 def run_benchmarks(output_dir, mode):
-    for reqs_per_second in [200, 300, 400, 500]:
-        for delta_graph_size in [128, 256, 512, 1024, 2048]:
-            run_benchmark(reqs_per_second, delta_graph_size, output_dir, mode)
+    for reqs_per_second in [200, 500]:
+        run_benchmark(reqs_per_second, output_dir, mode)
 
 
 if __name__ == '__main__':
-    # Get the output directory from the command line
-    if len(sys.argv) != 3:
-        print("Usage: python runBenchmarks.py <output-directory> <mode>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Run benchmarks for random workers.')
+    parser.add_argument('output_dir', type=str, help='The output directory for benchmark logs and recordings.')
+    parser.add_argument('mode', type=str, choices=['default', 'big-data', 'streaming'], help='The mode to run the benchmarks in.')
 
-    output_dir = sys.argv[1]
+    args = parser.parse_args()
+
+    output_dir = args.output_dir
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     mode = None
-    mode_arg = sys.argv[2]
-    if mode_arg == "default":
+    if args.mode == "default":
         mode = default_mode
-    elif mode_arg == "big-data":
+    elif args.mode == "big-data":
         mode = big_data_mode
-    elif mode_arg == "streaming":
+    elif args.mode == "streaming":
         mode = streaming_mode
-    else:
-        print("Invalid mode. Choose one of: default, big-data, streaming")
-        sys.exit(1)
 
     run_benchmarks(output_dir, mode)
